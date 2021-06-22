f2482b420b386dc7ca8ccc9bd7632ba1e6cf2d34
easymagic <nnamware@yahoo.com>
2021-06-22 14:54:44 +0100

    np

--- app/Providers/CrudScafoldService.php
+++ app/Providers/CrudScafoldService.php
@@ -28,7 +28,8 @@ class CrudScafoldService extends ServiceProvider
         //
         $this->publishes([
                 __DIR__ . '/../../app/Services/'=>base_path('app/Services/'),
-                __DIR__ . '/../../app/Console/Commands/'=>base_path('app/Console/Commands/')
+                __DIR__ . '/../../app/Console/Commands/'=>base_path('app/Console/Commands/'),
+                __DIR__ . '/../../config/filesystems.php'=>base_path('config/filesystems.php')
         ]);
 
 
